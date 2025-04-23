55df973b2e4c1064c28058b2b790ddbd41ffcd1a
Enzo <enzo.giaquinta@isep.edu.ar>
2025-04-23 18:43:53 -0300

    agregado bloque port a app.py y modificado requirements

--- app.py
+++ app.py
@@ -153,3 +153,8 @@ def eliminar_mensaje(id):
 @app.errorhandler(404)
 def error_404(error):
     return render_template("error404.html", error=error), 404
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
